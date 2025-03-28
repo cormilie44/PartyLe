41ddf64a3382442e26e57db934c563bb2499f3e9
Yahya Karacan <100614601+cormilie44@users.noreply.github.com>
2025-03-28 21:28:34 +0000

    Auto commit

--- src/pages/profile-screen.tsx
+++ src/pages/profile-screen.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Card, { CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "/components/ui/card"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "/components/ui/card"
 import Button from "/components/ui/button"
 import { ArrowRight } from 'lucide-react'
 
