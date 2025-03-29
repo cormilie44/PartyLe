229c38b9ee3c9183467a86fabd03806aefed4165
Yahya Karacan <100614601+cormilie44@users.noreply.github.com>
2025-03-29 10:20:19 +0000

    Yeni Vite bağımlılıkları için React modülü ve harita dosyası eklendi

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    host: '0.0.0.0',  // Dışarıya açmak için
+    port: 5173,       // Varsayılan port, ihtiyaca göre değiştirilebilir
+    open: true,       // Projeyi tarayıcıda otomatik açar
+  },
   css: {
     preprocessorOptions: {
       tailwindcss: {},
